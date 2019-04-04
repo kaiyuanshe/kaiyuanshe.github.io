66e621ce62205fab22aeb0233f4b5716c730a578
TechQuery <shiy007@qq.com>
2019-04-05 01:59:43 +0800

    [ Add ]  Files & Calendar widgets of Activity layout

--- themes/Electron/scripts/index.js
+++ themes/Electron/scripts/index.js
@@ -1,7 +1,50 @@
+const { VEvent } = require('icalendar');
+
+
 hexo.extend.helper.register('echo', console.log);
 
 
 hexo.extend.helper.register('has_category',  (post, name) =>
 
   Array.from((post.categories || '').data,  ({ name }) => name).includes( name )
+);
+
+
+const type_map = {
+  doc:  'word',
+  docx: 'word',
+  xls:  'excel',
+  xlsx: 'excel',
+  ppt:  'powerpoint',
+  pptx: 'powerpoint'
+};
+
+hexo.extend.helper.register('file_type',  path => {
+
+  const type = path.split('.').slice(-1)[0].toLowerCase();
+
+  return type_map[type] || type;
+});
+
+
+hexo.extend.helper.register(
+  'eventOf',  (title, description, start, end, location) => {
+
+    const event = new VEvent();
+
+    event.setSummary( title );
+
+    event.setDescription( description );
+
+    event.setDate(new Date(start), new Date(end));
+
+    event.setLocation( location );
+
+    return event;
+  }
+);
+
+hexo.extend.helper.register('toDataURI',  (data, type = '') =>
+
+  `data:${type};base64,${Buffer.from( data ).toString('base64')}`
 );
