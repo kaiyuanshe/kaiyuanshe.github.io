870dd11a640e7b9e673ea56d5c479d407d607e2e
TechQuery <shiy2008@gmail.com>
2019-08-04 20:41:12 +0800

    [Fix] Several details

--- themes/Electron/source/common.js
+++ themes/Electron/source/common.js
@@ -2,6 +2,9 @@ $( document ).ready(function () {
 
   $('.blog-post table, .blog-index table').addClass('table').each(function () {
 
-    if (! this.tHead.textContent.trim())  $(this.tHead).remove();
+    if (! this.tHead.textContent.trim())
+      $(this.tHead).remove();
+    else
+      $('th[align="center"]', this.tHead).addClass('text-center');
   });
 });
