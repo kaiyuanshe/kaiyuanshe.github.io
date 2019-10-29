52c05ca58cceb8bcfee5d08d3c59d277bafa829e
TechQuery <shiy007@qq.com>
2019-10-29 23:28:53 +0800

    [add] HighLight.js

--- themes/Electron/source/common.js
+++ themes/Electron/source/common.js
@@ -1,6 +1,8 @@
+hljs.initHighlightingOnLoad();
+
 $( document ).ready(function () {
 
-  $('.blog-post table, .blog-index table').addClass('table').each(function () {
+  $('section > table, main > table').addClass('table').each(function () {
 
     if (! this.tHead.textContent.trim())
       $(this.tHead).remove();
