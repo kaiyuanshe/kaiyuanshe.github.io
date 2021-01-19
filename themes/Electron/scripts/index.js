80c9eb5cbdef5bd3650392a8a0bd6703a2f7576a
TechQuery <shiy007@qq.com>
2021-01-19 20:52:39 +0800

    [add] urlFor() utility method

--- themes/Electron/scripts/index.js
+++ themes/Electron/scripts/index.js
@@ -1,81 +1,76 @@
 const { VEvent } = require('icalendar');
 
-
 hexo.extend.helper.register('echo', console.log);
 
 hexo.extend.helper.register('groupBy', (list, grouper) => {
-
   const data = {};
 
   for (const item of list) {
-
-    let keys = (grouper instanceof Function) ? grouper(item) : item[grouper];
+    let keys = grouper instanceof Function ? grouper(item) : item[grouper];
 
     if (!(keys instanceof Array)) keys = [keys];
 
-    for (const key of new Set(keys))
-      (data[key] = data[key] || []).push(item);
+    for (const key of new Set(keys)) (data[key] = data[key] || []).push(item);
   }
 
   return data;
 });
 
-hexo.extend.helper.register('getPosts', ({pagination, filter}) => {
-
-  const {order_by, per_page} = hexo.config.index_generator;
+hexo.extend.helper.register('getPosts', ({ pagination, filter }) => {
+  const { order_by, per_page } = hexo.config.index_generator;
 
   var { data } = hexo.locals.get('posts').sort(order_by);
 
-  if (filter instanceof Function) data = data.filter( filter );
+  if (filter instanceof Function) data = data.filter(filter);
 
   return data.slice(0, pagination ? per_page : Infinity);
 });
 
-hexo.extend.helper.register('hasCategory',  (post, name) =>
-
-  Array.from((post.categories || '').data,  ({ name }) => name).includes( name )
+hexo.extend.helper.register('hasCategory', (post, name) =>
+  Array.from((post.categories || '').data, ({ name }) => name).includes(name)
 );
 
-
 const type_map = {
-  doc:  'word',
+  doc: 'word',
   docx: 'word',
-  xls:  'excel',
+  xls: 'excel',
   xlsx: 'excel',
-  ppt:  'powerpoint',
-  pptx: 'powerpoint'
+  ppt: 'powerpoint',
+  pptx: 'powerpoint',
 };
 
-hexo.extend.helper.register('fileType',  path => {
-
+hexo.extend.helper.register('fileType', (path) => {
   var type = path.split('.');
 
-  if (! type[1]) return;
+  if (!type[1]) return;
 
   type = type.slice(-1)[0].toLowerCase();
 
   return type_map[type] || type;
 });
 
-
-hexo.extend.helper.register('toDataURI',  (data, type = '') =>
-
-  `data:${type};base64,${Buffer.from( data ).toString('base64')}`
+hexo.extend.helper.register('urlFor', (path, base) =>
+  /^(\w+:)?\/\//.test(path) ? path : base + path
 );
 
+hexo.extend.helper.register(
+  'toDataURI',
+  (data, type = '') =>
+    `data:${type};base64,${Buffer.from(data).toString('base64')}`
+);
 
 hexo.extend.helper.register(
-  'eventOf',  (title, description, start, end, location) => {
-
+  'eventOf',
+  (title, description, start, end, location) => {
     const event = new VEvent();
 
-    event.setSummary( title );
+    event.setSummary(title);
 
-    event.setDescription( description );
+    event.setDescription(description);
 
     event.setDate(new Date(start), new Date(end));
 
-    event.setLocation( location );
+    event.setLocation(location);
 
     return event;
   }
