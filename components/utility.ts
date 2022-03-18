26320e69e07eb2f0c75e78676d04aa217149b3d0
TechQuery <shiy007@qq.com>
2022-03-19 00:15:22 +0800

    [fix] AMap loading key [optimize] update Upstream packages

--- components/utility.ts
+++ components/utility.ts
@@ -2,6 +2,12 @@ import { config } from '@amap/amap-react';
 
 config.key = process.env.NEXT_PUBLIC_AMAP_KEY!;
 
+if (typeof window !== 'undefined')
+  // @ts-ignore
+  window._AMapSecurityConfig = {
+    securityJsCode: process.env.NEXT_PUBLIC_AMAP_SECRET,
+  };
+
 export const VariantColors = [
   'primary',
   'secondary',
