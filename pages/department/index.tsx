c2f53930515c80d8f3826221d4a13063d8b5d054
TechQuery <shiy2008@gmail.com>
2022-10-18 03:26:07 +0800

    [optimize] Label & Tooltip of Department Tree chart

--- pages/department/index.tsx
+++ pages/department/index.tsx
@@ -1,81 +1,29 @@
 import dynamic from 'next/dynamic';
-import { groupBy } from 'web-utility';
-import { computed } from 'mobx';
-import { observer } from 'mobx-react';
-import { PureComponent } from 'react';
 import { Container, Button } from 'react-bootstrap';
-import { Loading } from 'idea-react';
-import { TreeSeriesProps } from 'echarts-jsx';
 
 import PageHead from '../../components/PageHead';
-import groupStore from '../../models/Group';
 
-const TreeChart = dynamic(() => import('../../components/TreeChart'), {
-  ssr: false,
-});
+const DepartmentTree = dynamic(
+  () => import('../../components/DepartmentTree'),
+  { ssr: false },
+);
 
-@observer
-export default class DepartmentPage extends PureComponent {
-  componentDidMount() {
-    groupStore.getAll();
-  }
+export default function DepartmentPage() {
+  return (
+    <Container className="py-5 text-center">
+      <PageHead title="组织机构" />
 
-  @computed
-  get treeData(): TreeSeriesProps['data'] {
-    const { 职能: departments, 项目: projects } = groupBy(
-      groupStore.allItems,
-      'type',
-    );
+      <h1>开源社组织机构</h1>
 
-    return [
-      {
-        name: '开源社',
-        children: [
-          {
-            name: '理事会',
-            children: [
-              {
-                name: '执行委员会',
-                collapsed: false,
-                children: departments?.map(({ name }) => ({ name: name + '' })),
-              },
-              {
-                name: '项目委员会',
-                collapsed: false,
-                children: projects?.map(({ fullName }) => ({
-                  name: fullName + '',
-                })),
-              },
-            ],
-          },
-          { name: '顾问委员会' },
-          { name: '法律咨询委员会' },
-        ],
-      },
-    ];
-  }
+      <DepartmentTree />
 
-  render() {
-    const { downloading } = groupStore;
-
-    return (
-      <Container className="py-5 text-center">
-        <PageHead title="组织机构" />
-
-        <h1>开源社组织机构</h1>
-
-        {downloading > 0 && <Loading />}
-
-        <TreeChart data={this.treeData} />
-
-        <Button
-          size="lg"
-          target="_blank"
-          href="https://kaiyuanshe.feishu.cn/share/base/shrcnfO89tYlYIjZpS5PXJBaK2f"
-        >
-          成为志愿者
-        </Button>
-      </Container>
-    );
-  }
+      <Button
+        size="lg"
+        target="_blank"
+        href="https://kaiyuanshe.feishu.cn/share/base/shrcnfO89tYlYIjZpS5PXJBaK2f"
+      >
+        成为志愿者
+      </Button>
+    </Container>
+  );
 }
