f9a98f2e4fca7870b9185aeedb9ed476247e9fe2
DarrenJoe <491361994@qq.com>
2023-01-10 20:56:00 +0800

    style: Adjust the article listing style

--- components/Activity/Card.tsx
+++ components/Activity/Card.tsx
@@ -5,6 +5,7 @@ import { TimeDistance, text2color } from 'idea-react';
 import { TimeOption } from '../data';
 import { blobURLOf } from '../../models/Base';
 import type { Activity } from '../../pages/api/activity';
+import styles from '../../styles/CoverImg.module.less';
 
 export interface ActivityCardProps extends Activity {
   className?: string;
@@ -21,31 +22,31 @@ export const ActivityCard: FC<ActivityCardProps> = ({
   image,
 }: ActivityCardProps) => (
   <Card className={`shadow-sm ${className}`}>
-    <Card.Img
-      variant="top"
-      style={{ height: '30vh', objectFit: 'cover' }}
-      src={blobURLOf(image)}
-    />
+    <div className={styles.coverImgContainer}>
+      <div className="cover-img-wrapper">
+        <Card.Img className="cover-img" src={blobURLOf(image)} />
+      </div>
+    </div>
     <Card.Body className="d-flex flex-column">
-      <Card.Title as="h3" className="h4 flex-fill">
+      <Card.Title as="h5" className="h5 flex-fill">
         <a
-          className="text-decoration-none text-secondary"
+          className="text-decoration-none text-secondary overTwoLineText"
           href={link as string}
         >
           {name}
         </a>
       </Card.Title>
 
-      <Row className="mt-24">
+      <Row className="mt-2 flex-fill">
         <Col className="text-start">
-          <Card.Text className="mt-4 text-truncate" title={location + ''}>
-            <span className="me-3">{city}</span>
+          <Card.Text className="mt-1 text-truncate " title={location + ''}>
+            <span className="me-1">{city}</span>
             {location}
           </Card.Text>
         </Col>
       </Row>
-      <Row as="footer" className="small mt-3">
-        <Col>
+      <Row as="footer" className="flex-fill small mt-1">
+        <Col xs={7} sm={7} xl={7}>
           {(organizers as string[])?.map(organizer => (
             <Badge
               key={organizer}
@@ -58,12 +59,8 @@ export const ActivityCard: FC<ActivityCardProps> = ({
             </Badge>
           ))}
         </Col>
-        <Col className="text-end">
-          <TimeDistance
-            className="me-3"
-            {...TimeOption}
-            date={startTime as number}
-          />
+        <Col className="text-end" xs={5} sm={5} xl={5}>
+          <TimeDistance {...TimeOption} date={startTime as number} />
         </Col>
       </Row>
     </Card.Body>
--- components/Article/Card.tsx
+++ components/Article/Card.tsx
@@ -5,6 +5,7 @@ import { TimeDistance, text2color } from 'idea-react';
 import { TimeOption } from '../data';
 import { blobURLOf } from '../../models/Base';
 import type { BaseArticle } from '../../pages/api/article';
+import styles from '../../styles/CoverImg.module.less';
 
 export interface ArticleCardProps extends BaseArticle {
   className?: string;
@@ -20,32 +21,32 @@ export const ArticleCard: FC<ArticleCardProps> = ({
   publishedAt,
 }: ArticleCardProps) => (
   <Card className={`shadow-sm ${className}`}>
-    <Card.Img
-      variant="top"
-      style={{ height: '30vh', objectFit: 'cover' }}
-      src={blobURLOf(image)}
-    />
-    <Card.Body>
-      <Card.Title as="h3" className="h4">
+    <div className={styles.coverImgContainer}>
+      <div className="cover-img-wrapper">
+        <Card.Img className="cover-img" src={blobURLOf(image)} />
+      </div>
+    </div>
+    <Card.Body className="d-flex flex-column">
+      <Card.Title as="h5" className="h5 flex-fill">
         <a
-          className="text-decoration-none text-secondary"
+          className="text-decoration-none text-secondary overTwoLineText"
           href={`/article/${alias}`}
         >
           {title}
         </a>
       </Card.Title>
 
-      <Row className="mt-3">
+      <Row className="mt-2 flex-fill">
         <Col
           as="a"
-          className="text-decoration-none text-end"
+          className="text-decoration-none text-end text-truncate align-self-end"
           href={`/search?keywords=${author}`}
         >
           {author}
         </Col>
       </Row>
-      <Row as="footer" className="small mt-3">
-        <Col>
+      <Row as="footer" className="flex-fill small mt-1">
+        <Col xs={8} sm={8} xl={8}>
           {(tags + '').split(/\s+/).map(name => (
             <Badge
               as="a"
@@ -58,12 +59,8 @@ export const ArticleCard: FC<ArticleCardProps> = ({
             </Badge>
           ))}
         </Col>
-        <Col className="text-end">
-          <TimeDistance
-            className="me-3"
-            {...TimeOption}
-            date={publishedAt as number}
-          />
+        <Col className="text-end" xs={4} sm={4} xl={4}>
+          <TimeDistance {...TimeOption} date={publishedAt as number} />
         </Col>
       </Row>
     </Card.Body>
