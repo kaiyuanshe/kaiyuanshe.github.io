687f1c0be12bf3bc2b2e518c0fecc8b7a4049b2a
Five-great <43601963+Five-great@users.noreply.github.com>
2023-03-12 12:12:30 +0800

    Update en-US.ts

--- translation/en-US.ts
+++ translation/en-US.ts
@@ -102,5 +102,5 @@ export default {
   last_work_group: 'Last Work Group',
   last_project_group: 'Last Project Group',
   next_term_plan: 'Next Term Plan',
-  expert_committee: 'expert committee',
+  expert_committee: 'Expert Committee',
 } as const;
