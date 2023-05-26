80bb7f18078c1155117d3b6065fff590d5f28f08
luojiyin <luojiyin@hotmail.com>
2023-05-26 17:42:11 +0800

    fix: step error and i18n

--- translation/en-US.ts
+++ translation/en-US.ts
@@ -137,7 +137,7 @@ export default {
     'Remember: You must read and understand the license agreement you choose',
   filter_option: 'filter option',
   option_undefined: 'Not required',
-  step_x: ({ step }: { step: number }) => `step ${step + 1}`,
+  step_x: ({ step }: { step: number }) => `step ${step}`,
   license_score: 'score',
   feature_popularity: 'Popularity',
   feature_reuse_condition: 'Reuse Condition',
--- translation/zh-CN.ts
+++ translation/zh-CN.ts
@@ -25,7 +25,7 @@ export default {
   open_source_treasure_box: '开源百宝箱',
   Web_polyfill_CDN: 'Web 标准补丁 CDN',
   open_source_mirror: '开源镜像站',
-  license_tool: '开源协议选择器',
+  license_tool: '开源许可证选择器',
 
   no_more: '没有更多',
   load_more: '加载更多……',
