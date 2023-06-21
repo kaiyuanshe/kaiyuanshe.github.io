8f91c7ed4808dcc37480d970f74c5436c28f7e49
luojiyin <luojiyin@hotmail.com>
2023-06-21 12:13:04 +0800

    demo

--- pages/activity/[id]/agenda/[agendaId]/invitation.tsx
+++ pages/activity/[id]/agenda/[agendaId]/invitation.tsx
@@ -1,8 +1,15 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import { FC } from 'react';
+import { Container, Row, Col, Image, Card } from 'react-bootstrap';
+
+import { DefaultImage } from '../../../../api/lark/file/[id]';
 
 import { Activity, ActivityModel } from '../../../../../models/Activity';
 import { Agenda } from '../../../../../models/Agenda';
+import PageHead from '../../../../../components/PageHead';
+import { blobURLOf } from '../../../../../models/Base';
+
+import styles from '../../../../../styles/invitation.module.less';
 
 export const getServerSideProps: GetServerSideProps<
   { activity: Activity; agenda: Agenda },
@@ -24,12 +31,35 @@ export const getServerSideProps: GetServerSideProps<
 const Invitation: FC<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > = ({ activity, agenda }) => {
+  console.log('activity.id', activity);
+  console.log('agenda', agenda);
   return (
-    <div>
-      <h1>Invitation Page</h1>
-      <p>Activity ID: {activity.id}</p>
-      <p>Agenda ID: {agenda.id}</p>
-    </div>
+    <Container className={styles.invitationBG}>
+      <h1>{activity.name}</h1>
+      <h2>{activity.city}</h2>
+      <h2>{activity.location}</h2>
+
+      <h2>
+        🕒 {new Date(+agenda.startTime!).toLocaleString()} ~{' '}
+        {new Date(+agenda.endTime!).toLocaleString()}
+      </h2>
+      <h3>{agenda.title}</h3>
+
+      <div className="d-flex">
+        {(agenda.mentorAvatars as unknown as TableCellAttachment[])?.map(
+          file => (
+            <Card.Img
+              key={file.attachmentToken}
+              className="object-fit-cover"
+              style={{ height: '20rem' }}
+              loading="lazy"
+              src={blobURLOf([file])}
+            />
+          ),
+        )}
+      </div>
+      <h3>👨‍🎓 {(agenda.mentors as string[]).join(' ')}</h3>
+    </Container>
   );
 };
 
