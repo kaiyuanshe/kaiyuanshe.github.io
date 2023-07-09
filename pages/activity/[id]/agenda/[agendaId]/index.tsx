0d32ff7f1a67df970385f8fd0a96149ba4387327
South Drifted <shiy2008@gmail.com>
2023-07-10 03:29:51 +0800

    [add] Agenda People component & Normalize Text Array method (#124)

--- pages/activity/[id]/agenda/[agendaId]/index.tsx
+++ pages/activity/[id]/agenda/[agendaId]/index.tsx
@@ -1,16 +1,22 @@
+import { TableCellValue } from 'mobx-lark';
 import { observer } from 'mobx-react';
-import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
-import React, { PureComponent } from 'react';
-import { Badge, Container } from 'react-bootstrap';
+import { InferGetServerSidePropsType } from 'next';
+import { PureComponent } from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
 
+import { AgendaPeople } from '../../../../../components/Activity/Agenda/People';
 import { CommentBox } from '../../../../../components/CommentBox';
+import PageHead from '../../../../../components/PageHead';
 import { Activity, ActivityModel } from '../../../../../models/Activity';
 import { Agenda } from '../../../../../models/Agenda';
+import { blobURLOf } from '../../../../../models/Base';
+import { i18n } from '../../../../../models/Translation';
+import { withTranslation } from '../../../../api/base';
 
-export const getServerSideProps: GetServerSideProps<
-  { activity: Activity; agenda: Agenda },
-  { id: string; agendaId: string }
-> = async ({ params }) => {
+export const getServerSideProps = withTranslation<
+  { id: string; agendaId: string },
+  { activity: Activity; agenda: Agenda }
+>(async ({ params }) => {
   const activityStore = new ActivityModel(),
     { id, agendaId } = params!;
   const activity = await activityStore.getOne(id + ''),
@@ -19,47 +25,69 @@ export const getServerSideProps: GetServerSideProps<
   return {
     props: { activity, agenda },
   };
-};
+});
+
+const { t } = i18n;
 
 @observer
 export default class AgendaDetailPage extends PureComponent<
   InferGetServerSidePropsType<typeof getServerSideProps>
 > {
   render() {
+    const { id: activityId, name } = this.props.activity;
     const {
+      id,
       forum,
       title,
       mentors,
+      mentorAvatars,
+      mentorPositions,
+      mentorSummaries,
       startTime,
       endTime,
-      summary = '暂无内容',
+      summary = t('no_data'),
     } = this.props.agenda;
 
-    const mentorPosition: any = this.props.agenda.mentorPosition;
-    const mentorPositionList = mentorPosition
-      ? mentorPosition[0]?.text.split(',')
-      : [];
-
     return (
-      <Container className="pt-3">
-        <header>
-          <div className="text-end text-success">{forum}</div>
-          <h2>{title}</h2>
-          <span>{mentors}</span>
-          {mentorPositionList.map((position: string) => (
-            <Badge key={position} bg="dark" className="mx-2">
-              {position}
-            </Badge>
-          ))}
-          <div>
-            🕒 {new Date(+startTime!).toLocaleString()} ~{' '}
-            {new Date(+endTime!).toLocaleString()}
-          </div>
-        </header>
-        <main className="my-2">{summary}</main>
-        <footer>
-          <CommentBox category="General" categoryId="DIC_kwDOB88JLM4COLSV" />
-        </footer>
+      <Container className="pt-5">
+        <PageHead title={`${title} - ${name}`} />
+
+        <Row>
+          <Col xs={12} sm={9}>
+            <header>
+              <div className="d-flex align-items-center justify-content-between">
+                <h1>{title}</h1>
+                <Button
+                  size="sm"
+                  variant="warning"
+                  href={`/activity/${activityId}/agenda/${id}/invitation`}
+                >
+                  {t('share')}
+                </Button>
+              </div>
+              <div className="d-flex gap-3">
+                <div className="text-success">{forum}</div>
+                <div>
+                  🕒 {new Date(+startTime!).toLocaleString()} ~{' '}
+                  {new Date(+endTime!).toLocaleString()}
+                </div>
+              </div>
+            </header>
+            <main className="my-2">{summary}</main>
+          </Col>
+          <Col xs={12} sm={3}>
+            <AgendaPeople
+              names={mentors as string[]}
+              avatars={(mentorAvatars as TableCellValue[]).map(file =>
+                blobURLOf([file] as TableCellValue),
+              )}
+              positions={mentorPositions as string[]}
+              summaries={mentorSummaries as string[]}
+            />
+          </Col>
+        </Row>
+
+        <CommentBox category="General" categoryId="DIC_kwDOB88JLM4COLSV" />
       </Container>
     );
   }
