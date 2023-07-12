9169b7a5beba6f4839ca43f97d60d254bfb65096
luojiyin <luojiyin@hotmail.com>
2023-07-12 16:14:48 +0800

    function component to class component

--- components/Activity/Agenda/Card.tsx
+++ components/Activity/Agenda/Card.tsx
@@ -1,82 +1,89 @@
 import { TableCellAttachment } from 'mobx-lark';
 import { observer } from 'mobx-react';
-import { FC } from 'react';
+import { Component } from 'react';
 import { Card, Carousel } from 'react-bootstrap';
 
 import { blobURLOf } from '../../../models/Base';
 import { AgendaToolbar, AgendaToolbarProps } from './Toolbar';
 
-export const AgendaCard: FC<AgendaToolbarProps> = observer(
-  ({
-    activityId,
-    id,
-    title,
-    mentors,
-    mentorAvatars,
-    startTime,
-    endTime,
-    ...props
-  }) => (
-    <Card className="h-100">
-      <div className="d-flex">
-        {(mentorAvatars as TableCellAttachment[])?.[1] ? (
-          <Carousel className="w-100">
-            {(mentorAvatars as unknown as TableCellAttachment[])?.map(file => (
-              <Carousel.Item key={file.attachmentToken}>
-                <Card.Img
-                  key={file.attachmentToken}
-                  className="m-auto object-fit-cover"
-                  style={{ height: '25rem' }}
-                  loading="lazy"
-                  src={blobURLOf([file])}
-                />
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        ) : (
-          (mentorAvatars as unknown as TableCellAttachment[])?.map(file => (
-            <Card.Img
-              key={file.attachmentToken}
-              className="object-fit-cover"
-              style={{ height: '25rem' }}
-              loading="lazy"
-              src={blobURLOf([file])}
-            />
-          ))
-        )}
-      </div>
+@observer
+export class AgendaCard extends Component<AgendaToolbarProps> {
+  render() {
+    const {
+      activityId,
+      id,
+      title,
+      mentors,
+      mentorAvatars,
+      startTime,
+      endTime,
+      ...props
+    } = this.props;
 
-      <Card.Body className="d-flex flex-column justify-content-end">
-        <Card.Title
-          as="a"
-          className="text-decoration-none text-secondary text-truncation-lines"
-          href={`/activity/${activityId}/agenda/${id}`}
-        >
-          {title}
-        </Card.Title>
+    return (
+      <Card className="h-100">
+        <div className="d-flex">
+          {(mentorAvatars as TableCellAttachment[])?.[1] ? (
+            <Carousel className="w-100">
+              {(mentorAvatars as unknown as TableCellAttachment[])?.map(
+                file => (
+                  <Carousel.Item key={file.attachmentToken}>
+                    <Card.Img
+                      key={file.attachmentToken}
+                      className="m-auto object-fit-cover"
+                      style={{ height: '25rem' }}
+                      loading="lazy"
+                      src={blobURLOf([file])}
+                    />
+                  </Carousel.Item>
+                ),
+              )}
+            </Carousel>
+          ) : (
+            (mentorAvatars as unknown as TableCellAttachment[])?.map(file => (
+              <Card.Img
+                key={file.attachmentToken}
+                className="object-fit-cover"
+                style={{ height: '25rem' }}
+                loading="lazy"
+                src={blobURLOf([file])}
+              />
+            ))
+          )}
+        </div>
 
-        <ul className="list-unstyled">
-          <li>👨‍🎓 {(mentors as string[]).join(' ')}</li>
-          <li>
-            🕒 {new Date(+startTime!).toLocaleString()} ~{' '}
-            {new Date(+endTime!).toLocaleString()}
-          </li>
-        </ul>
-      </Card.Body>
-      <Card.Footer className="d-flex justify-content-end">
-        <AgendaToolbar
-          {...{
-            activityId,
-            id,
-            title,
-            mentors,
-            mentorAvatars,
-            startTime,
-            endTime,
-            ...props,
-          }}
-        />
-      </Card.Footer>
-    </Card>
-  ),
-);
+        <Card.Body className="d-flex flex-column justify-content-end">
+          <Card.Title
+            as="a"
+            className="text-decoration-none text-secondary text-truncation-lines"
+            href={`/activity/${activityId}/agenda/${id}`}
+          >
+            {title}
+          </Card.Title>
+
+          <ul className="list-unstyled">
+            <li>👨‍🎓 {(mentors as string[]).join(' ')}</li>
+            <li>
+              🕒 {new Date(+startTime!).toLocaleString()} ~{' '}
+              {new Date(+endTime!).toLocaleString()}
+            </li>
+          </ul>
+        </Card.Body>
+        <Card.Footer className="d-flex justify-content-end">
+          <AgendaToolbar
+            {...{
+              activityId,
+              id,
+              title,
+              mentors,
+              mentorAvatars,
+              startTime,
+              endTime,
+              ...props,
+            }}
+          />
+        </Card.Footer>
+      </Card>
+    );
+  }
+}
