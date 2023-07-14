e2ad42ce12e9a3e0712de541ff7720ea19078c95
TechQuery <shiy2008@gmail.com>
2023-07-15 03:56:41 +0800

    [add] Personnel list & model

--- pages/person/[name].tsx
+++ pages/person/[name].tsx
@@ -1,40 +1,54 @@
 import { text2color } from 'idea-react';
 import { InferGetServerSidePropsType } from 'next';
-import { Badge, Container, Image } from 'react-bootstrap';
+import { PureComponent } from 'react';
+import { Badge, Col, Container, Image, Row } from 'react-bootstrap';
+import { formatDate } from 'web-utility';
 
 import PageHead from '../../components/PageHead';
 import { Person, PersonModel } from '../../models/Person';
+import { Personnel, PersonnelModel } from '../../models/Personnel';
 import { withErrorLog, withTranslation } from '../api/base';
 
 export const getServerSideProps = withErrorLog<
   { name: string },
-  { person: Person }
+  { person: Person; personnels: Personnel[] }
 >(
-  withTranslation(async ({ params }) => {
-    const [person] = await new PersonModel().getList({ name: params!.name });
+  withTranslation(async ({ params: { name } = {} }) => {
+    const [person] = await new PersonModel().getList({ name });
 
-    return { props: JSON.parse(JSON.stringify({ person })) };
+    if (!person) return { notFound: true, props: {} };
+
+    const personnels = await new PersonnelModel().getList({ recipient: name });
+
+    return {
+      props: JSON.parse(JSON.stringify({ person, personnels })),
+    };
   }),
 );
 
-export default function PersonDetailPage({
-  person,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  return (
-    <Container className="py-5">
-      <PageHead title={person.name as string} />
+export default class PersonDetailPage extends PureComponent<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> {
+  renderProfile = ({
+    name,
+    gender,
+    city,
+    email,
+    website,
+    github = 'https://github.com/kaiyuanshe',
+    skills,
+    summary,
+  }: Person) => (
+    <>
+      <Image src={`${github}.png`} />
 
-      <Image src={`${person.github || 'https://github.com/kaiyuanshe'}.png`} />
-
-      <h1>{person.name}</h1>
+      <h1>{name}</h1>
       <ul>
         <li>
-          <Badge bg={text2color(person.gender as string, ['light'])}>
-            {person.gender}
-          </Badge>
+          <Badge bg={text2color(gender as string, ['light'])}>{gender}</Badge>
         </li>
         <li>
-          {(person.skills as string[]).map(skill => (
+          {(skills as string[]).map(skill => (
             <Badge
               key={skill}
               className="me-2"
@@ -44,27 +58,76 @@ export default function PersonDetailPage({
             </Badge>
           ))}
         </li>
-        <li>🗺 {person.city}</li>
+        <li>🗺 {city}</li>
         <li>
-          📬{' '}
-          <a href={person.email as string}>
-            {(person.email as string).split(':')[1]}
-          </a>
+          📬 <a href={email as string}>{(email as string).split(':')[1]}</a>
         </li>
         <li>
           🖥{' '}
-          <a target="_blank" href={person.website as string} rel="noreferrer">
-            {person.website}
+          <a target="_blank" href={website as string} rel="noreferrer">
+            {website}
           </a>
         </li>
         <li>
           ⌨{' '}
-          <a target="_blank" href={person.github as string} rel="noreferrer">
-            {person.github}
+          <a target="_blank" href={github as string} rel="noreferrer">
+            {github}
           </a>
         </li>
       </ul>
-      <article>{person.summary}</article>
-    </Container>
+      <article>{summary}</article>
+    </>
   );
+
+  renderPersonnel = ({
+    id,
+    createdAt,
+    type,
+    department,
+    position,
+    award,
+    reason,
+  }: Personnel) => (
+    <li key={id as string} className="mb-3">
+      <details>
+        <summary>
+          <div className="d-inline-flex align-items-center gap-2">
+            <time dateTime={new Date(createdAt as number).toJSON()}>
+              {formatDate(createdAt as number, 'YYYY-MM-DD')}
+            </time>
+            <Badge bg={text2color(type as string, ['light'])}>{type}</Badge>
+            {award ? (
+              <span>{award}</span>
+            ) : (
+              <>
+                <span>{department}</span>
+                <span>{position}</span>
+              </>
+            )}
+          </div>
+        </summary>
+
+        {reason}
+      </details>
+    </li>
+  );
+
+  render() {
+    const { person, personnels } = this.props;
+
+    return (
+      <Container className="py-5">
+        <PageHead title={person.name as string} />
+
+        <Row>
+          <Col xs={12} sm={4}>
+            {this.renderProfile(person)}
+          </Col>
+          <Col xs={12} sm={8} as="ol" className="list-unstyled">
+            {personnels.map(this.renderPersonnel)}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
