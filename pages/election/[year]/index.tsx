1af8e065a1bf79bf3fd333b0d8b785fcb506d9bb
South Drifted <shiy2008@gmail.com>
2023-07-17 02:48:32 +0800

    [refactor] rewrite Election card & list page with new Data structure (#133)

--- pages/election/[year]/index.tsx
+++ pages/election/[year]/index.tsx
@@ -3,41 +3,39 @@ import { observer } from 'mobx-react';
 import { InferGetServerSidePropsType } from 'next';
 import { FC } from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
+import { isEmpty } from 'web-utility';
 
-import { ElectorCard } from '../../../components/Election/ElectorCard';
+import { ElectionCard } from '../../../components/Election/ElectorCard';
 import PageHead from '../../../components/PageHead';
-import {
-  ELECTION_BASE_ID,
-  ELECTION_TABLE_ID,
-  ElectionTarget,
-  Elector,
-  ElectorModel,
-} from '../../../models/Elector';
+import { ElectionTarget, PersonnelModel } from '../../../models/Personnel';
 import { i18n } from '../../../models/Translation';
-import { withRoute, withTranslation } from '../../api/base';
+import { withErrorLog, withRoute, withTranslation } from '../../api/base';
 
 export const getServerSideProps = withRoute<
   { year: string },
-  { electorGroup: Record<ElectionTarget, Elector[]> }
+  Pick<PersonnelModel, 'group'>
 >(
-  withTranslation(async ({ params }) => {
-    const electorStore = new ElectorModel(ELECTION_BASE_ID, ELECTION_TABLE_ID);
+  withErrorLog(
+    withTranslation(async ({ params }) => {
+      const group = await new PersonnelModel().getGroup(+params!.year);
 
-    const electorGroup = await electorStore.getGroup(+params!.year);
-
-    return { props: { electorGroup } };
-  }),
+      return {
+        notFound: isEmpty(group),
+        props: JSON.parse(JSON.stringify({ group })),
+      };
+    }),
+  ),
 );
 
 const { t } = i18n;
 
 const ElectionPage: FC<InferGetServerSidePropsType<typeof getServerSideProps>> =
-  observer(({ route, electorGroup }) => {
+  observer(({ route, group }) => {
     const { year } = route.params!;
 
     const title = `${year} ${t('election')}`;
 
-    const passed = +new Date(`${year}-03-01`) <= Date.now();
+    const passed = +year < new Date().getFullYear();
 
     return (
       <Container>
@@ -47,24 +45,16 @@ const ElectionPage: FC<InferGetServerSidePropsType<typeof getServerSideProps>> =
 
         <div className="d-flex justify-content-center gap-3 my-3">
           <Button
-            variant="success"
-            target="_blank"
-            href="https://kaiyuanshe.feishu.cn/share/base/form/shrcnbqCtt9p9QkYFO1YAUGnMJh"
-            disabled={passed}
-          >
-            选民登记
-          </Button>
-          <Button
             variant="warning"
             target="_blank"
-            href="https://kaiyuanshe.feishu.cn/share/base/form/shrcnbqCtt9p9QkYFO1YAUGnMJh"
+            href="https://kaiyuanshe.feishu.cn/share/base/form/shrcnVxuTa41nb80lCy7H9GXrkb?prefill_%E6%8F%90%E5%90%8D%E7%B1%BB%E5%9E%8B=%E4%BB%BB%E5%91%BD&prefill_%E7%9B%AE%E6%A0%87%E9%83%A8%E9%97%A8=%E7%90%86%E4%BA%8B%E4%BC%9A&prefill_%E7%9B%AE%E6%A0%87%E8%81%8C%E4%BD%8D=%E7%90%86%E4%BA%8B"
             disabled={passed}
           >
-            理事竞选
+            理事提名
           </Button>
           <Button
             target="_blank"
-            href="https://kaiyuanshe.feishu.cn/share/base/form/shrcnbqCtt9p9QkYFO1YAUGnMJh"
+            href="https://kaiyuanshe.feishu.cn/share/base/form/shrcn20SIKJgdsHH9AEGEMYFPwf?prefill_%E7%94%B3%E8%AF%B7%E7%B1%BB%E5%9E%8B=%E4%BB%BB%E5%91%BD&prefill_%E7%94%B3%E8%AF%B7%E8%81%8C%E4%BD%8D=%E6%AD%A3%E5%BC%8F%E6%88%90%E5%91%98"
             disabled={passed}
           >
             正式成员申请
@@ -72,33 +62,35 @@ const ElectionPage: FC<InferGetServerSidePropsType<typeof getServerSideProps>> =
           <Button
             variant="danger"
             target="_blank"
-            href="https://kaiyuanshe.feishu.cn/share/base/form/shrcnehPTKXtDsoKVfv0mKGZwge"
+            href="https://kaiyuanshe.feishu.cn/share/base/form/shrcnVYqyX5w8wTNiCLeH7Ziy1g"
             disabled={passed}
           >
             选举投票
           </Button>
         </div>
 
-        {Object.entries(electorGroup).map(([target, list]) => (
+        {Object.entries(group).map(([target, list]) => (
           <section key={target}>
             <h2 className="my-5 text-center">
-              {textJoin(t(target as ElectionTarget), t('candidate'))}
+              {textJoin(t(target as ElectionTarget) || '', t('candidate'))}
             </h2>
 
             <Row as="ul" className="list-unstyled g-4" xs={1} sm={2} lg={3}>
               {list
                 .sort(
                   (
-                    { councilVoteCount: a1, regularVoteCount: a2 },
-                    { councilVoteCount: b1, regularVoteCount: b2 },
-                  ) => +b1! - +a1! || +b2! - +a2!,
+                    { approvers: a1, rejecters: a2 },
+                    { approvers: b1, rejecters: b2 },
+                  ) =>
+                    (b1 as string[])?.length - (a1 as string[])?.length ||
+                    (a2 as string[])?.length - (b2 as string[])?.length,
                 )
-                .map(item => (
-                  <Col as="li" key={item.id + ''}>
-                    <ElectorCard
+                .map(({ id, ...item }, index) => (
+                  <Col as="li" key={id + ''}>
+                    <ElectionCard
                       className="h-100"
-                      href={`/election/${year}/elector/${item.id}`}
                       {...item}
+                      order={index + 1}
                     />
                   </Col>
                 ))}
