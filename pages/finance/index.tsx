259918108fe8fb8de6f5cd64da777843276b9292
luojiyin <luojiyin@hotmail.com>
2023-08-08 20:49:10 +0800

    fix: del base

--- pages/finance/index.tsx
+++ pages/finance/index.tsx
@@ -1,16 +1,17 @@
 import { observer } from 'mobx-react';
-import { ScrollList } from 'mobx-restful-table';
+import { Column, RestTable } from 'mobx-restful-table';
 import { InferGetServerSidePropsType } from 'next';
+import { compose } from 'next-ssr-middleware';
 import { FC, PureComponent } from 'react';
+import { Container } from 'react-bootstrap';
 
 import PageHead from '../../components/PageHead';
 import { Bill, BillModel } from '../../models/Bill';
 import { i18n } from '../../models/Translation';
-import { withErrorLog, withTranslation } from '../api/base';
 
 const { t } = i18n;
 
-export const getServerSideProps = withErrorLog<{ id: string }>(
+export const getServerSideProps = compose<{ id: string }>(
   async ({ params }) => {
     const billStrore = new BillModel();
     const bill = await billStrore.getOne(params!.id);
@@ -24,4 +25,24 @@ export const getServerSideProps = withErrorLog<{ id: string }>(
 @observer
 export default class BillDetail extends PureComponent<
   InferGetServerSidePropsType<typeof getServerSideProps>
-> {}
+> {
+  render() {
+    const { bill } = this.props;
+
+    return (
+      <Container style={{ height: '91vh' }}>
+        <PageHead title="财务公开" />
+
+        <RestTable
+          className="h-100 text-center"
+          striped
+          hover
+          editable
+          deletable
+          store={bill}
+          onCheck={console.log}
+        />
+      </Container>
+    );
+  }
+}
