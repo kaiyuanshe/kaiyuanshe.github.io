f184641b06b9a31fc228e80c57da57b7e9c13345
luojiyin <luojiyin@hotmail.com>
2023-08-16 16:20:50 +0800

    refactor: finance page

--- pages/activity/[id]/finance.tsx
+++ pages/activity/[id]/finance.tsx
@@ -1,10 +1,93 @@
-import { TableCellValues } from 'mobx-lark';
+import { computed } from 'mobx';
 import { observer } from 'mobx-react';
+import { Column, RestTable } from 'mobx-restful-table';
 import { InferGetServerSidePropsType } from 'next';
 import { cache, compose, errorLogger, translator } from 'next-ssr-middleware';
 import { PureComponent } from 'react';
 import { Container } from 'react-bootstrap';
 
+import PageHead from '../../../components/PageHead';
 import { Activity, ActivityModel } from '../../../models/Activity';
+import { Bill, BillModel } from '../../../models/Bill';
 import { i18n } from '../../../models/Translation';
-import PageHead from '../../components/PageHead';
+
+export const getServerSideProps = compose<
+  { id: string },
+  {
+    activity: Activity;
+    currentMeta: ActivityModel['currentMeta'];
+    bills: Bill[];
+  }
+>(
+  cache(),
+  errorLogger,
+  translator(i18n),
+
+  async ({ params }) => {
+    const activityStore = new ActivityModel();
+    const activity = await activityStore.getOne(params!.id);
+    const billStore = activityStore.currentBill;
+
+    const [bills] = await Promise.all([activityStore.currentBill!.getList()]);
+    const { currentMeta } = activityStore;
+    return {
+      props: { activity, bills, billStore, currentMeta },
+    };
+  },
+);
+
+const { t } = i18n;
+
+@observer
+export default class BillDetailPage extends PureComponent<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> {
+  @computed
+  get columns(): Column<Bill>[] {
+    return [
+      {
+        renderHead: t('bill_id'),
+        type: 'bill_id',
+        key: 'id',
+      },
+      {
+        renderHead: t('bill_createAt'),
+        type: 'bill_createAt',
+        key: 'createdAt',
+      },
+      {
+        renderHead: t('bill_createBy'),
+        type: 'bill_createBy',
+        key: 'createdBy',
+      },
+      {
+        renderHead: t('bill_type'),
+        type: 'bill_type',
+        key: 'type',
+      },
+    ];
+  }
+
+  render() {
+    const { bills, billStore } = this.props;
+
+    return (
+      <Container style={{ height: '91vh' }}>
+        <PageHead title="财务公开" />
+        <h1>财务公开</h1>
+
+        <RestTable
+          className="h-100 text-center"
+          bill_id
+          bill_createAt
+          bill_createBy
+          bill_type
+          translator={i18n}
+          store={billStore}
+          columns={this.columns}
+          defaultData={bills}
+        />
+      </Container>
+    );
+  }
+}
