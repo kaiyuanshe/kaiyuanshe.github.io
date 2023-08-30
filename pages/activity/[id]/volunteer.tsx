e27d3fb842d137840de97e8fb4a7389c79bdb3ee
luojiyin <luojiyin@hotmail.com>
2023-08-30 18:17:03 +0800

    save

--- pages/activity/[id]/volunteer.tsx
+++ pages/activity/[id]/volunteer.tsx
@@ -1,9 +1,7 @@
-import { Loading } from 'idea-react';
-import { computed, observable } from 'mobx';
+import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import { InferGetServerSidePropsType } from 'next';
 import {
-  cache,
   compose,
   errorLogger,
   RouteProps,
@@ -11,17 +9,44 @@ import {
   translator,
 } from 'next-ssr-middleware';
 import { PureComponent } from 'react';
-import { Badge, Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 import PageHead from '../../../components/PageHead';
 import { ActivityModel } from '../../../models/Activity';
+import { Person, PersonModel } from '../../../models/Person';
 import { i18n } from '../../../models/Translation';
 
-export const getServerSideProps = compose<{ id: string }>(
-  cache(),
+interface PersonListPageProps extends RouteProps {
+  list: Person[];
+}
+
+export const getServerSideProps = compose<{ id: string }, PersonListPageProps>(
   router,
   errorLogger,
   translator(i18n),
+
+  async () => {
+    const list = await new PersonModel().getList({}, 1);
+
+    return { props: { list } as PersonListPageProps };
+  },
 );
 
 const { t } = i18n;
+
+@observer
+export default class VolunteerPage extends PureComponent<
+  InferGetServerSidePropsType<typeof getServerSideProps>
+> {
+  @observable
+  activityStore?: ActivityModel;
+
+  render() {
+    console.log('this.activityStore', this.activityStore);
+    return (
+      <Container style={{ height: '91vh' }}>
+        <PageHead />
+      </Container>
+    );
+  }
+}
