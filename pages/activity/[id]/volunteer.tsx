76108d69efb681cd2c1d63bc2e992c64b5c6e63a
luojiyin <luojiyin@hotmail.com>
2023-08-31 10:40:01 +0800

    feat: volunteer page

--- pages/activity/[id]/volunteer.tsx
+++ pages/activity/[id]/volunteer.tsx
@@ -1,4 +1,4 @@
-import { observable } from 'mobx';
+import { makeObservable,observable } from 'mobx';
 import { observer } from 'mobx-react';
 import { InferGetServerSidePropsType } from 'next';
 import {
@@ -20,29 +20,44 @@ interface PersonListPageProps extends RouteProps {
   list: Person[];
 }
 
+type VolunteerDetailPageProps = RouteProps<{ id: string }>;
+
 export const getServerSideProps = compose<{ id: string }, PersonListPageProps>(
   router,
   errorLogger,
   translator(i18n),
 
-  async () => {
-    const list = await new PersonModel().getList({}, 1);
+  // async () => {
+  //   const list = await new PersonModel().getList({}, 1);
 
-    return { props: { list } as PersonListPageProps };
-  },
+  //   return { props: { list } as PersonListPageProps };
+  // },
 );
 
 const { t } = i18n;
 
 @observer
-export default class VolunteerPage extends PureComponent<
-  InferGetServerSidePropsType<typeof getServerSideProps>
-> {
+export default class VolunteerPage extends PureComponent<VolunteerDetailPageProps> {
+  constructor(props: VolunteerDetailPageProps) {
+    super(props);
+    makeObservable(this);
+  }
+
   @observable
-  activityStore?: ActivityModel;
+  activityStore?: ActivityModel = undefined;
+
+  @observable
+  PersonStore?: PersonModel = undefined;
+
+  async componentDidMount() {
+    const { id } = this.props.route.params!;
+    this.activityStore = new ActivityModel();
+    await this.activityStore.getOne(id);
+    this.PersonStore = this.activityStore.currentPerson;
+  }
 
   render() {
-    console.log('this.activityStore', this.activityStore);
+    console.log('this.activityStore', this.PersonStore);
     return (
       <Container style={{ height: '91vh' }}>
         <PageHead />
