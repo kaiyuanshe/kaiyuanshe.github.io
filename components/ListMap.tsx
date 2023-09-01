c0f2fcb8ea10f5ea89e64ab5d9f4f485f437b8b0
taitouxiangshang <huoyueyuyuan@126.com>
2023-09-01 11:19:46 +0800

    update bootstrap icon

--- components/ListMap.tsx
+++ components/ListMap.tsx
@@ -94,11 +94,11 @@ export class ListMap extends PureComponent<ListMapProps> {
                   rel="noreferrer"
                 >
                   <svg
+                    className="bi bi-geo-alt-fill"
                     xmlns="http://www.w3.org/2000/svg"
                     width="24"
                     height="24"
                     fill="green"
-                    className="bi bi-geo-alt-fill"
                     viewBox="0 0 16 16"
                   >
                     <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z" />
