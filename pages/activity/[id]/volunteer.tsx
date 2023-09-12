eb7c914af9033359779c17793c2c756745fb0767
luojiyin <luojiyin@hotmail.com>
2023-09-12 22:50:33 +0800

    save

--- pages/activity/[id]/volunteer.tsx
+++ pages/activity/[id]/volunteer.tsx
@@ -2,10 +2,10 @@ import { Loading } from 'idea-react';
 import { makeObservable, observable } from 'mobx';
 import { observer } from 'mobx-react';
 import {
+  cache,
   compose,
   errorLogger,
   RouteProps,
-  router,
   translator,
 } from 'next-ssr-middleware';
 import { PureComponent } from 'react';
@@ -15,38 +15,43 @@ import { MemberCard } from '../../../components/Member/Card';
 import { MemberTitle } from '../../../components/Member/Title';
 import PageHead from '../../../components/PageHead';
 import { i18n } from '../../../models//Base/Translation';
-import { ActivityModel } from '../../../models/Activity';
+import { Activity, ActivityModel } from '../../../models/Activity';
 import { Staff, StaffModel } from '../../../models/Activity/Staff';
 import { blobURLOf } from '../../../models/Base';
 
-type VolunteerDetailPageProps = RouteProps<{ id: string }>;
-
-export const getServerSideProps = compose<
-  { id: string },
-  VolunteerDetailPageProps
->(router, errorLogger, translator(i18n));
+interface VolunteerPageProps {
+  activity: Activity;
+  staff: Staff;
+}
 
 const { t } = i18n;
 
+export const getServerSideProps = compose<{ id: string }, VolunteerPageProps>(
+  cache(),
+  errorLogger,
+
+  translator(i18n),
+  async ({ params }) => {
+    const activityStore = new ActivityModel();
+    const { id } = params!;
+    const activity = await activityStore.getOne(id);
+    const staff = await activityStore.currentStaff?.getGroup({
+      role: '志愿者',
+    });
+
+    return {
+      props: JSON.parse(JSON.stringify({ activity, staff })),
+    };
+  },
+);
+
 @observer
-export default class VolunteerPage extends PureComponent<VolunteerDetailPageProps> {
-  constructor(props: VolunteerDetailPageProps) {
+export default class VolunteerPage extends PureComponent<VolunteerPageProps> {
+  constructor(props: VolunteerPageProps) {
     super(props);
     makeObservable(this);
   }
 
-  @observable
-  activityStore?: ActivityModel = undefined;
-
-  @observable
-  staffStore?: StaffModel = undefined;
-
-  async componentDidMount() {
-    const { id } = this.props.route.params!;
-    this.activityStore = new ActivityModel();
-    await this.activityStore.getOne(id);
-    this.activityStore.currentStaff?.getGroup({ role: '志愿者' });
-  }
   renderVolunteers = ({ id, name, avatar }: Staff) => (
     <li
       key={id as string}
@@ -57,28 +62,27 @@ export default class VolunteerPage extends PureComponent<VolunteerDetailPageProp
   );
 
   render() {
-    const { activityStore } = this;
-    const list = activityStore?.currentStaff?.allItems;
+    const { activity, staff } = this.props;
 
-    const loading = activityStore?.downloading || 0;
-    const { name = '' } = activityStore?.currentOne || {};
+    // const loading =  staff.length || 0;
+    const { name = '' } = activity;
 
     return (
       <Container className="py-5">
         <PageHead title={t('volunteer') + '-' + name} />
         <h1 className="text-center">{name + ' ' + t('volunteer')}</h1>
-        {loading > 0 && <Loading />}
-
-        <section>
+        {/* {loading > 0 && <Loading />} */}
+        <h4>{JSON.stringify(staff)}</h4>
+        {/* <section>
           <MemberTitle
             className="my-5"
             title={t('volunteer')}
-            count={list?.length}
+            count={staff?.length}
           />
           <ul className="list-unstyled d-flex flex-wrap grap-3">
-            {list?.map(this.renderVolunteers)}
+            {staff?.map(this.renderVolunteers)}
           </ul>
-        </section>
+        </section> */}
       </Container>
     );
   }
