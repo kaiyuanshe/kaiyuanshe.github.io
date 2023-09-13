0decc622ba37bbb88d7606d5ed92e43522ed0998
taitouxiangshang <huoyueyuyuan@126.com>
2023-09-13 21:07:48 +0800

    fix err reference

--- components/Community/MemberList.tsx
+++ components/Community/MemberList.tsx
@@ -1,23 +1,13 @@
 import { FC } from 'react';
 import { Col, Row } from 'react-bootstrap';
 
-import { Person } from '../../models/Personnel/Person';
+import { Person } from '../../models/Community/Person';
 import { CommunityMemberCard } from './MemberCard';
 
 export const CommunityMemberList: FC<{ defaultData: Person[] }> = ({
   defaultData,
 }) => (
   <Row as="section" xs={1} sm={2} xl={5} className="g-3 my-4">
-    {defaultData.map(item => (
-      <Col key={item.name + ''}>
-        <CommunityMemberCard {...item} />
-      </Col>
-    ))}
-    {defaultData.map(item => (
-      <Col key={item.name + ''}>
-        <CommunityMemberCard {...item} />
-      </Col>
-    ))}
     {defaultData.map(item => (
       <Col key={item.name + ''}>
         <CommunityMemberCard {...item} />
