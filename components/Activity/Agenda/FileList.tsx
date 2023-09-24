69266b27d08e7fe66a411c826c94446516fda4a4
taitouxiangshang <huoyueyuyuan@126.com>
2023-09-24 19:07:14 +0800

    add import

--- components/Activity/Agenda/FileList.tsx
+++ components/Activity/Agenda/FileList.tsx
@@ -1,4 +1,6 @@
+import { text2color } from 'idea-react';
 import { TableCellAttachment } from 'mobx-lark';
+import { observer } from 'mobx-react';
 import { FilePreview } from 'mobx-restful-table';
 import { FC } from 'react';
 import { Badge } from 'react-bootstrap';
@@ -7,19 +9,21 @@ import { i18n } from '../../../models/Base/Translation';
 
 const { t } = i18n;
 
-export const FileList: FC<{ data: TableCellAttachment[] }> = ({ data }) => (
-  <section>
-    <h2>{t('file_download')}</h2>
-    <ol>
-      {data.map(({ id, name, mimeType, attachmentToken }) => (
-        <li key={id + ''}>
-          <FilePreview
-            type={mimeType}
-            path={`/api/lark/file/${attachmentToken}`}
-          />
-          <Badge>{name}</Badge>
-        </li>
-      ))}
-    </ol>
-  </section>
+export const FileList: FC<{ data: TableCellAttachment[] }> = observer(
+  ({ data }) => (
+    <section>
+      <h2>{t('file_download')}</h2>
+      <ol>
+        {data.map(({ id, name, mimeType, attachmentToken }) => (
+          <li key={id + ''}>
+            <FilePreview
+              type={mimeType}
+              path={`/api/lark/file/${attachmentToken}`}
+            />
+            <Badge bg={text2color(name, ['light'])}>{name}</Badge>
+          </li>
+        ))}
+      </ol>
+    </section>
+  ),
 );
