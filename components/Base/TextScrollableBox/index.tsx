74145ab5a7f7c12d1aeeead4098334660690ae08
Sleek Zheng <slick.zheng@opus.pro>
2023-11-01 14:23:25 +0800

    Update components/Base/TextScrollableBox/index.tsx
    Co-authored-by: South Drifted <shiy2008@gmail.com>

--- components/Base/TextScrollableBox/index.tsx
+++ components/Base/TextScrollableBox/index.tsx
@@ -11,19 +11,10 @@ export const TextScrollableBox: FC<TextScrollableBoxProps> = ({
   children,
   width,
   duration,
-}: TextScrollableBoxProps) => {
-  let boxStyle = {};
-  if (width) {
-    boxStyle = Object.assign(boxStyle, { width });
-  }
-  if (duration) {
-    boxStyle = Object.assign(boxStyle, { '--duration': duration });
-  }
-  return (
-    <div className={style.box} style={boxStyle}>
+}) => (
+    <div className={style.box} style={{ width, '--duration': duration }>
       <div className={style.scrollWrap}>
         <div className={style.scrollItem}>{children}</div>
       </div>
     </div>
   );
-};
