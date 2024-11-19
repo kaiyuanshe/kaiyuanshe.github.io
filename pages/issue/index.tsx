a5e26b0cab4f4e464f49161a349a6f42f3b358aa
luojiyin <luojiyin@hotmail.com>
2024-11-19 22:01:24 +0800

    code clear

--- pages/issue/index.tsx
+++ pages/issue/index.tsx
@@ -8,9 +8,8 @@ import {
   translator,
 } from 'next-ssr-middleware';
 import { FC } from 'react';
-import { Container } from 'react-bootstrap';
+import { Button,Container } from 'react-bootstrap';
 
-import { SearchBar } from '../../components/Base/SearchBar';
 import { IssueListLayout } from '../../components/Issue/List';
 import { PageHead } from '../../components/Layout/PageHead';
 import { i18n, t } from '../../models/Base/Translation';
@@ -26,18 +25,23 @@ export const getServerSideProps = compose<{}, IssuePageProps>(
   translator(i18n),
   async ({ query }) => {
     const list = await new IssueModel().getList(query);
-    return { props: { list } as IssuePageProps };
+
+    return { props: JSON.parse(JSON.stringify({ list })) as IssuePageProps };
   },
 );
 
 const IssuePage: FC<IssuePageProps> = observer(({ route: { query }, list }) => (
   <Container>
-    <PageHead title={t('issue')} />
+    <PageHead title={t('issue_box')} />
 
-    <header className="d-flex flex-wrap justify-content-around align-items-center my-4">
-      <h1 className="my-4">{t('issue')}</h1>
-      <SearchBar action="/search/issue" />
-    </header>
+    <Button
+      className="my-3"
+      size="lg"
+      target="_blank"
+      href="https://kaiyuanshe.feishu.cn/share/base/form/shrcn6Yd39PMtEbIaT4uiliAeJF"
+    >
+      {t('issue_box')}
+    </Button>
     <ScrollList
       translator={i18n}
       store={issueStore}
