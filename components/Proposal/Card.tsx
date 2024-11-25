afc045ae00f20b1b8918ef637dbde88bb458220a
luojiyin <luojiyin@hotmail.com>
2024-11-25 10:46:54 +0800

    show more info

--- components/Proposal/Card.tsx
+++ components/Proposal/Card.tsx
@@ -2,6 +2,7 @@ import { TimeDistance } from 'idea-react';
 import type { FC } from 'react';
 import { Card, Col, Row } from 'react-bootstrap';
 
+import Issue from '../../models/Governance/Issue';
 import type { Proposal } from '../../models/Governance/Proposal';
 
 export interface ProposalCardProps extends Proposal {
@@ -14,6 +15,7 @@ export const ProposalCard: FC<ProposalCardProps> = ({
   contentURL,
   createdBy,
   meetings,
+  issues,
 }) => (
   <Card
     className={`shadow-sm ${className}`}
@@ -21,14 +23,37 @@ export const ProposalCard: FC<ProposalCardProps> = ({
   >
     <Card.Body className="d-flex-column">
       <Card.Title as="h3" className="h5 flex-fill">
-        <a href={contentURL as string} target="_blank" rel="noreferrer">
+        <a
+          href={contentURL as string}
+          className="text-decoration-none"
+          target="_blank"
+          rel="noreferrer"
+        >
           {title as string}
         </a>
       </Card.Title>
+      <Row>
+        <a
+          href={`/member/${createdBy}`}
+          className="text-decoration-none text-muted hover:text-primary"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {createdBy as string}
+        </a>
+      </Row>
     </Card.Body>
     <Card.Footer className="d-flex flex-wrap justify-content-between align-items-center">
-      <div>{createdBy as string}</div>
-      <div>{meetings as string}</div>
+      <Row>
+        <Col className="text-decoration-none align-self-end" xs={10}>
+          {`相关会议: ${meetings as string}`}
+        </Col>
+      </Row>
+      <Row>
+        <Col className="text-decoration-none align-self-end" xs={10}>
+          {`相关提议: ${issues as string}`}
+        </Col>
+      </Row>
     </Card.Footer>
   </Card>
 );
